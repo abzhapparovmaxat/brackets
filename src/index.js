543b8f41cdecc73ccded62748532c49cc48a1dca
abzhapparovmaxat <abzhapparovmaxat@gmail.com>
2019-03-20 16:26:43 +0600

    fix: function check in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  
+  let regExp = '';
+  bracketsConfig.forEach(function(item, i, arr) {
+
+    (/[A-Za-zА-Яа-яЁё0-9]/.test(bracketsConfig[i][0]))
+    ? regExp += bracketsConfig[i][0]
+    : regExp += '\\' + bracketsConfig[i][0];
+
+    (/[A-Za-zА-Яа-яЁё0-9]/.test(bracketsConfig[i][1]))
+    ? regExp += bracketsConfig[i][1]
+    : regExp += '\\' + bracketsConfig[i][1];
+  
+  });
+  
+  str = str.match(new RegExp('[' + regExp + ']','g')).join('');
+  
+  let brackets = '';
+  let foundBrackets = true;
+  while(foundBrackets) {
+    
+    foundBrackets = false;
+    bracketsConfig.forEach(function(item, i, arr) {
+      brackets = bracketsConfig[i][0] + bracketsConfig[i][1];
+      if(str.indexOf(brackets) + 1) {
+        str = str.split(brackets).join('');      
+        foundBrackets = true;
+      }
+    });
+    
+  };
+  
+  return (str=='') ? true : false;
+  
 }
